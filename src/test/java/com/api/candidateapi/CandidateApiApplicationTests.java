5d86545b4d4de7e2679c11ac3e77e1f5abe4f500
robelshanbel <52001231+robelshanbel@users.noreply.github.com>
2021-10-13 07:14:38 -0700

    Update CandidateApiApplicationTests.java

--- src/test/java/com/api/candidateapi/CandidateApiApplicationTests.java
+++ src/test/java/com/api/candidateapi/CandidateApiApplicationTests.java
@@ -1,25 +1,13 @@
 package com.api.candidateapi;
 
-import com.api.candidateapi.Model.Candidates;
-import com.api.candidateapi.Repo.CandidateRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import  java.util.*;
+
 @SpringBootTest
 class CandidateApiApplicationTests {
-@Autowired
-    CandidateRepository candidateRepository;
 
     @Test
     void contextLoads() {
     }
-
-    void readAllCandidate(){
-        List<Candidates>list=candidateRepository.findAll();
-        Assertions.assertThat(list).size().isGreaterThan(0);
-    }
-
 
 }
